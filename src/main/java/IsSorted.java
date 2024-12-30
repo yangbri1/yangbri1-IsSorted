258b3a2af31aa6562ec9040311c3215d16518fd7
Brian Yang <yangbri1@msu.edu>
2024-12-30 02:20:56 +0000

    Checking if a given array of int 'arr' is sorted using control flow 'while' loop alongside some if-else statements

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -1,3 +1,6 @@
+// import 'Arrays' class from java.util package
+import java.util.Arrays;
+
 public class IsSorted {
     /**
      * Return true if arr is sorted, smallest to largest, false otherwise.
@@ -7,6 +10,30 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        // Arrays.sort(arr);
+        // if(Arrays.compare(arr, Arrays.sort(arr))){
+
+        // }
+
+        // boolean defValue = false;
+        
+        // initialize iterator 'i' for while loop below
+        int i = 1;
+        // loop through given array 'arr' of integers
+        while(i < arr.length){
+            // if current index value is greater than last
+            if(arr[i-1] < arr[i]){
+                // continue on
+            }
+            // otw array 'arr' is not in Array.sort() fashion
+            else{
+                // return false (implicitly break out of loop in doing so)
+                return false;
+            }
+            // increment by 1  to continue loop
+            ++i;
+        }
+        // if reach end of 'arr' w/o throwing out an 'false' anywhere --- array 'arr' is sorted
+        return true;
     }
 }
